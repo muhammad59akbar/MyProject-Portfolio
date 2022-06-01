db8e437852f10719410afbed2a66f4a3f59796b3
muhammadrizkiakbar <muhammad59akbar@gmail.com>
2022-06-02 04:10:02 +0700

    add component slider

---
+++ src/components/contactpage/Slider/slider-api.js
@@ -0,0 +1,13 @@
+import image1 from '../../../assets/slider/slider1.png';
+import image2 from '../../../assets/slider/slider2.png';
+
+export const imageslider = [
+    {
+        id: 1,
+        image: image1
+    },
+    {
+        id: 2,
+        image: image2
+    }
+]
